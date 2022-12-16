06fea26c854c35e703ad9739ee9171816558131a
Khanhtt.dev <khanhtt.dev@gmail.com>
2022-12-16 16:50:47 +0700

    Create Server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.json("Welcome");
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
