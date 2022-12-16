3f826221e4dc733f1d77341190a70f28aa927272
Khanhtt.dev <khanhtt.dev@gmail.com>
2022-12-16 16:58:20 +0700

    Not Found Middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +10,8 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	res.json("Welcome");
-});
+// middleware
+app.use(notFoundMiddleware);
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
