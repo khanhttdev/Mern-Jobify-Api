a9eab6b433df220e5bd3ee2c3e5af09650caa0ec
Khanhtt.dev <khanhtt.dev@gmail.com>
2022-12-21 09:58:20 +0700

    Register controller

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,11 @@
+import { StatusCodes } from "http-status-codes";
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-	const errorStatus = err.status || 500;
-	const errorMessage = err.message || "Something went wrong!";
-	return res.status(errorStatus).json({
-		success: false,
-		status: errorStatus,
-		message: errorMessage,
-		stack: err.stack,
-	});
+	const defaultErr = {
+		statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+		message: "Something went wrong , Please try again later",
+	};
+	res.status(defaultErr.statusCode).json({ msg: err });
 };
 
 export default errorHandlerMiddleware;
